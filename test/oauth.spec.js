333546177fe72eda2486dee71997afe53faa2dbf
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-06-21 15:29:45 -0400

    end-user authorization

--- test/oauth.spec.js
+++ test/oauth.spec.js
@@ -1,7 +1,8 @@
 var expect = require('chai').expect;
 var assert = require('chai').assert;
+var qs = require('querystring');
 
-var withings = require('../index');
+var oauth = require('../lib/oauth');
 
 before(function (done) {
     // clear db and other stuff
@@ -16,12 +17,39 @@ describe('Withings OAuth functionality:', function() {
             oauthConsumerSecret: '2d91017e9212bb0ec47f4de08b8423809537c9fd9f3c0d85d28c5d6b3db',
             oauthCallback: 'amida-tech.com'
         };
-        withings.oauth.getRequestToken(options, function(err, res, body) {
+        oauth.getRequestToken(options, function(err, res, body) {
             if (err) {
                 done(err);
             } else {
-                console.log(body);
+                expect(body).to.contain('oauth_token');
+                expect(body).to.contain('oauth_token_secret');
                 done();
+            }
+        });
+    });
+    
+    it('authorize an end-user', function(done) {
+        var options = {
+            oauthConsumerKey: '1b2fff4f26bfeb92b642fdcf54709e0954d9d21935d3444febf3e1884e973',
+            oauthConsumerSecret: '2d91017e9212bb0ec47f4de08b8423809537c9fd9f3c0d85d28c5d6b3db',
+            oauthCallback: 'amida-tech.com'
+        };
+        oauth.getRequestToken(options, function(err, res, body) {
+            if (err) {
+                done(err);
+            } else {
+                expect(body).to.be.a('string');
+                var tokens = qs.parse(body);
+                options.oauthRequestTokenKey = tokens.oauth_token;
+                options.oauthRequestTokenSecret = tokens.oauth_token_secret;
+                oauth.authorizeUser(options, function(err, res, body) {
+                    if (err) {
+                        done(err);
+                    } else {
+                        expect(body).to.contain('My Withings account');
+                        done();
+                    }
+                });
             }
         });
     });
