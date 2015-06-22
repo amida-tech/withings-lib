1eeb1795746831f1cc4b69ce7ed6f11bbe8406db
Jacob Sachs <jacob.s.sachs@gmail.com>
2015-06-22 19:51:24 -0400

    add coveralls

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,81 @@
 'use strict';
 module.exports = function (grunt) {
-  // Show elapsed time at the end
-  require('time-grunt')(grunt);
-  // Load all grunt tasks
-  require('load-grunt-tasks')(grunt);
+    // Show elapsed time at the end
+    require('time-grunt')(grunt);
+    // Load all grunt tasks
+    require('load-grunt-tasks')(grunt);
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        reporter: require('jshint-stylish'),
-        browser: true,
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: false,
-        boss: true,
-        eqnull: true,
-        node: true,
-        expr: true,
-        globals: {
-            'xit': true,
-            'xdescribe': true,
-            'it': true,
-            'describe': true,
-            'before': true,
-            'after': true,
-            'done': true
+    grunt.initConfig({
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish'),
+                browser: true,
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: false,
+                boss: true,
+                eqnull: true,
+                node: true,
+                expr: true,
+                globals: {
+                    'xit': true,
+                    'xdescribe': true,
+                    'it': true,
+                    'describe': true,
+                    'before': true,
+                    'after': true,
+                    'done': true
+                }
+            },
+            gruntfile: {
+                src: ['Gruntfile.js']
+            },
+            js: {
+                src: ['*.js', 'lib/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js']
+            }
+        },
+        jsbeautifier: {
+            files: ['Gruntfile.js', 'index.js', '*.js', 'lib/*.js', 'test/**/*.js'],
+            options: {
+                config: '.jsbeautifyrc'
+            }
+        },
+        mochacli: {
+            options: {
+                reporter: 'nyan',
+                bail: true
+            },
+            all: ['test/*.js']
+        },
+        shell: {
+            run_istanbul: {
+                command: "istanbul cover ./node_modules/mocha/bin/_mocha -- -R spec --recursive"
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            js: {
+                files: '<%= jshint.js.src %>',
+                tasks: ['jshint:js', 'mochacli']
+            },
+            test: {
+                files: '<%= jshint.test.src %>',
+                tasks: ['jshint:test', 'mochacli']
+            }
         }
-      },
-      gruntfile: {
-        src: ['Gruntfile.js']
-      },
-      js: {
-        src: ['*.js', 'lib/**/*.js']
-      },
-      test: {
-        src: ['test/**/*.js']
-      }
-    },
-    jsbeautifier: {
-        files: ['Gruntfile.js', 'index.js', '*.js', 'lib/*.js', 'test/**/*.js'],
-        options: {
-            config: '.jsbeautifyrc'
-        }
-    },
-    mochacli: {
-      options: {
-        reporter: 'nyan',
-        bail: true
-      },
-      all: ['test/*.js']
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      js: {
-        files: '<%= jshint.js.src %>',
-        tasks: ['jshint:js', 'mochacli']
-      },
-      test: {
-        files: '<%= jshint.test.src %>',
-        tasks: ['jshint:test', 'mochacli']
-      }
-    }
-  });
+    });
 
-  grunt.registerTask('default', ['jshint', 'mochacli']);
+    grunt.registerTask('default', ['jshint', 'jsbeautifier', 'mochacli']);
+    grunt.registerTask('coverage', ['shell:run_istanbul']);
 };
